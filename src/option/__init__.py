5381761692cb39a8b53c9e53b4efce3839a988b5
Michael Cummings <mgcummings@yahoo.com>
2025-09-17 11:25:12 -0700

    Use relative imports in `src/option/__init__.py` for consistency within the module.

--- src/option/__init__.py
+++ src/option/__init__.py
@@ -27,8 +27,8 @@
 
     Represents a None value.
 """
-from option_ import NONE, Option, Some, maybe
-from result import Err, Ok, Result
+from .option_ import NONE, Option, Some, maybe
+from .result import Err, Ok, Result
 
 __version__ = '2.1.0'
 __all__ = ['NONE', 'Option', 'Some', 'maybe', 'Result', 'Ok', 'Err', '__version__']
