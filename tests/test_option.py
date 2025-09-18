3c3129b5da2a8b945f6deaa36999f329413f0d06
Michael Cummings <mgcummings@yahoo.com>
2025-09-18 15:10:53 -0700

    Simplify and unify license headers across test files.

--- tests/test_option.py
+++ tests/test_option.py
@@ -1,32 +1,25 @@
-# MIT License
+# noqa: D100
+# Copyright © 2018-2022 Peijun Ma
+# Copyright © 2025 Michael Cummings <mgcummings@yahoo.com>
 #
-# Copyright (c) 2018-2022 Peijun Ma
+# Licensed under the MIT license
+# [MIT](https:#opensource.org/license/mit-0)
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Files in this project may not be copied, modified, or distributed except
+# according to those terms.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The full test of the license can be found in the project LICENSE.md file.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# SPDX-License-Identifier: MIT
+##############################################################################
 
 import pytest
-
 from option import NONE, Option, Some, maybe
+
 from tests.conftest import parametrize
 
 
-@parametrize('is_some', [True, False])
+@parametrize("is_some", [True, False])
 def test_no_init(is_some):
     with pytest.raises(TypeError):
         if is_some:
@@ -35,7 +28,7 @@ def test_no_init(is_some):
             Option(None, False)
 
 
-@parametrize('obj', [Option.Some(1), Some(1)])
+@parametrize("obj", [Option.Some(1), Some(1)])
 def test_some(obj):
     assert obj.is_some is True
     assert obj.is_none is False
@@ -43,7 +36,7 @@ def test_some(obj):
     assert obj._val == 1
 
 
-@parametrize('obj', [Option.NONE(), NONE])
+@parametrize("obj", [Option.NONE(), NONE])
 def test_none(obj):
     assert obj.is_some is False
     assert obj.is_none is True
@@ -51,19 +44,22 @@ def test_none(obj):
     assert obj._val is None
 
 
-@parametrize('obj,is_some', [
-    (maybe(0), True),
-    (maybe(0.0), True),
-    (maybe(''), True),
-    (maybe([]), True),
-    (maybe({}), True),
-    (maybe(()), True),
-    (maybe(set()), True),
-    (maybe(None), False),
-    (maybe(1), True),
-    (maybe(True), True),
-    (maybe(False), True)
-])
+@parametrize(
+    "obj,is_some",
+    [
+        (maybe(0), True),
+        (maybe(0.0), True),
+        (maybe(""), True),
+        (maybe([]), True),
+        (maybe({}), True),
+        (maybe(()), True),
+        (maybe(set()), True),
+        (maybe(None), False),
+        (maybe(1), True),
+        (maybe(True), True),
+        (maybe(False), True),
+    ],
+)
 def test_maybe(obj, is_some):
     if is_some:
         assert obj.is_some
@@ -71,31 +67,23 @@ def test_maybe(obj, is_some):
         assert obj.is_none
 
 
-@parametrize('obj,truth', [
-    (Some(1), True),
-    (NONE, False)
-])
+@parametrize("obj,truth", [(Some(1), True), (NONE, False)])
 def test_bool(obj, truth):
     assert bool(obj) is truth
 
 
-@parametrize('obj,raise_', [
-    (Some(1), False),
-    (NONE, True)
-])
+@parametrize("obj,raise_", [(Some(1), False), (NONE, True)])
 def test_except(obj, raise_):
     if raise_:
         with pytest.raises(ValueError):
-            obj.expect('asd')
+            obj.expect("asd")
     else:
-        assert obj.expect('asd') == 1
+        assert obj.expect("asd") == 1
 
 
-@parametrize('obj,raise_', [
-    (Some(1), False),
-    (Some(None), False),
-    (NONE, True)
-])
+@parametrize(
+    "obj,raise_", [(Some(1), False), (Some(None), False), (NONE, True)]
+)
 def test_value(obj, raise_):
     if raise_:
         with pytest.raises(ValueError):
@@ -106,11 +94,9 @@ def test_value(obj, raise_):
         assert obj.value == obj.unwrap() == obj._val
 
 
-@parametrize('obj,default,exp', [
-    (Some(0), 1, 0),
-    (Some(False), 1, False),
-    (NONE, 0, 0)
-])
+@parametrize(
+    "obj,default,exp", [(Some(0), 1, 0), (Some(False), 1, False), (NONE, 0, 0)]
+)
 def test_unwrap_or(obj, default, exp):
     assert obj.unwrap_or(default) == exp
     assert obj.unwrap_or_else(lambda: default) == exp
@@ -120,83 +106,107 @@ def _raise(*args):
     raise ValueError
 
 
-@parametrize('obj,func,exp', [
-    (Some(1), lambda x: x * 2, Some(2)),
-    (NONE, _raise, NONE),
-    (Some('asd'), len, Some(3))
-])
+@parametrize(
+    "obj,func,exp",
+    [
+        (Some(1), lambda x: x * 2, Some(2)),
+        (NONE, _raise, NONE),
+        (Some("asd"), len, Some(3)),
+    ],
+)
 def test_map(obj, func, exp):
     assert obj.map(func) == exp
 
 
-@parametrize('obj,func,default,exp', [
-    (Some(1), lambda x: x * 2, 100, 2),
-    (NONE, _raise, 100, 100),
-    (Some('asd'), len, 100, 3)
-])
+@parametrize(
+    "obj,func,default,exp",
+    [
+        (Some(1), lambda x: x * 2, 100, 2),
+        (NONE, _raise, 100, 100),
+        (Some("asd"), len, 100, 3),
+    ],
+)
 def test_map_or(obj, func, default, exp):
     assert obj.map_or(func, default) == exp
     assert obj.map_or_else(func, lambda: default) == exp
 
 
-@parametrize('obj,filt,exp', [
-    (Some(1), lambda x: x % 2 == 0, NONE),
-    (Some('asd'), lambda s: len(s) < 10, Some('asd')),
-    (NONE, lambda x: True, NONE),
-    (NONE, _raise, NONE)
-])
+@parametrize(
+    "obj,filt,exp",
+    [
+        (Some(1), lambda x: x % 2 == 0, NONE),
+        (Some("asd"), lambda s: len(s) < 10, Some("asd")),
+        (NONE, lambda x: True, NONE),
+        (NONE, _raise, NONE),
+    ],
+)
 def test_filter(obj, filt, exp):
     assert obj.filter(filt) == exp
 
 
-@parametrize('a,b,exp', [
-    (Some(1), NONE, NONE),
-    (NONE, Some(1), NONE),
-    (Some(2), Some('asd'), Some('asd')),
-    (NONE, NONE, NONE)
-])
+@parametrize(
+    "a,b,exp",
+    [
+        (Some(1), NONE, NONE),
+        (NONE, Some(1), NONE),
+        (Some(2), Some("asd"), Some("asd")),
+        (NONE, NONE, NONE),
+    ],
+)
 def test_and(a, b, exp):
     assert (a and b) == exp
 
 
-@parametrize('a,b,exp', [
-    (Some(1), Some(2), Some(1)),
-    (Some(1), NONE, Some(1)),
-    (NONE, Some(1), Some(1)),
-    (NONE, NONE, NONE),
-])
+@parametrize(
+    "a,b,exp",
+    [
+        (Some(1), Some(2), Some(1)),
+        (Some(1), NONE, Some(1)),
+        (NONE, Some(1), Some(1)),
+        (NONE, NONE, NONE),
+    ],
+)
 def test_or(a, b, exp):
     assert (a or b) == exp
 
 
-@parametrize('self,other', [
-    (Some(1), Some(1)),
-    (Some([1, 2]), Some([1, 2])),
-    (Some({'': 'asd'}), Some({'': 'asd'})),
-    (NONE, NONE)
-])
+@parametrize(
+    "self,other",
+    [
+        (Some(1), Some(1)),
+        (Some([1, 2]), Some([1, 2])),
+        (Some({"": "asd"}), Some({"": "asd"})),
+        (NONE, NONE),
+    ],
+)
 def test_eq(self, other):
     assert self == other
     assert not self == self._val
     assert not self._val == self
 
 
-@parametrize('self,other', [
-    (Some(1), Some(2)),
-    (NONE, Some(1)),
-    (Some(1), NONE),
-])
+@parametrize(
+    "self,other",
+    [
+        (Some(1), Some(2)),
+        (NONE, Some(1)),
+        (Some(1), NONE),
+    ],
+)
 def test_ne(self, other):
     assert self != other
     assert self != self._val
     assert self._val != self
 
 
-@parametrize('self,other', [
-    (Some(1), Some(2)),
-    (NONE, Some(0)),
-    (NONE, Some(False)),
-])
+@parametrize(
+    "self,other",
+    [
+        (Some(1), Some(2)),
+        (NONE, Some(0)),
+        (NONE, Some(False)),
+    ],
+)
 def test_lt_gt(self, other):
     assert self < other
     assert self <= other
@@ -209,14 +219,17 @@ def test_le_ge():
     assert NONE >= NONE
 
 
-@parametrize('self,other', [
-    (Some(None), Some(1)),
-    (Some(1), Some(None)),
-    (Some(1), Some('')),
-    (Some(''), Some(None)),
-    (Some(1), 1),
-    (1, Some(1))
-])
+@parametrize(
+    "self,other",
+    [
+        (Some(None), Some(1)),
+        (Some(1), Some(None)),
+        (Some(1), Some("")),
+        (Some(""), Some(None)),
+        (Some(1), 1),
+        (1, Some(1)),
+    ],
+)
 def test_lt_gt_type_error(self, other):
     with pytest.raises(TypeError):
         self < other
@@ -228,22 +241,24 @@ def test_lt_gt_type_error(self, other):
         self >= other
 
 
-@parametrize('obj,key,default,exp', [
-    (NONE, 1, None, NONE),
-    (NONE, 1, 1, Some(1)),
-    (Some({}), '', None, NONE),
-    (Some({'': ''}), '', None, Some('')),
-    (Some({'': ''}), 'aaa', 11, Some(11)),
-])
+@parametrize(
+    "obj,key,default,exp",
+    [
+        (NONE, 1, None, NONE),
+        (NONE, 1, 1, Some(1)),
+        (Some({}), "", None, NONE),
+        (Some({"": ""}), "", None, Some("")),
+        (Some({"": ""}), "aaa", 11, Some(11)),
+    ],
+)
 def test_get(obj, key, default, exp):
     assert obj.get(key, default) == exp
 
 
-@parametrize('obj1,obj2,eq', [
-    (Some(1), Some(1), True),
-    (NONE, NONE, True),
-    (Some(1), None, False)
-])
+@parametrize(
+    "obj1,obj2,eq",
+    [(Some(1), Some(1), True), (NONE, NONE, True), (Some(1), None, False)],
+)
 def test_hash(obj1, obj2, eq):
     if eq:
         assert hash(obj1) == hash(obj2)
--- tests/test_result.py
+++ tests/test_result.py
@@ -1,55 +1,45 @@
-# MIT License
-
-# Copyright (c) 2018-2022 Peijun Ma
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# noqa: D100
+# Copyright © 2018-2022 Peijun Ma
+# Copyright © 2025 Michael Cummings <mgcummings@yahoo.com>
+#
+# Licensed under the MIT license
+# [MIT](https:#opensource.org/license/mit-0)
+#
+# Files in this project may not be copied, modified, or distributed except
+# according to those terms.
+#
+# The full test of the license can be found in the project LICENSE.md file.
+#
+# SPDX-License-Identifier: MIT
+##############################################################################
 
 import pytest
+from option import NONE, Err, Ok, Result, Some
 
-from option import Err, NONE, Ok, Result, Some
 from tests.conftest import parametrize
 
 
-@parametrize('args', [
-    (1, True),
-    (0, False)
-])
+@parametrize("args", [(1, True), (0, False)])
 def test_no_init(args):
     with pytest.raises(TypeError):
         Result(*args)
 
 
-@parametrize('val', [0, None, {}, [], False])
+@parametrize("val", [0, None, {}, [], False])
 def test_factory_ok(val):
     res = Result.Ok(val)
     assert res._is_ok
     assert res._val == val
 
 
-@parametrize('err', [0, None, '', [], False])
+@parametrize("err", [0, None, "", [], False])
 def test_factory_err(err):
     res = Result.Err(err)
     assert not res._is_ok
     assert res._val == err
 
 
-@parametrize('ok', [True, False])
+@parametrize("ok", [True, False])
 def test_bool(ok):
     res = Result.Ok(1) if ok else Result.Err(1)
     assert bool(res) == ok
@@ -57,7 +47,7 @@ def test_bool(ok):
     assert res.is_err != ok
 
 
-@parametrize('ok', [True, False])
+@parametrize("ok", [True, False])
 def test_ok_err(ok):
     res = Result.Ok(1) if ok else Result.Err(1)
     if ok:
@@ -68,27 +58,24 @@ def test_ok_err(ok):
         assert res.err() == Some(1)
 
 
-@parametrize('obj,call,exp', [
-    (Ok(1), str, Ok('1')),
-    (Err(1), str, Err(1))
-])
+@parametrize("obj,call,exp", [(Ok(1), str, Ok("1")), (Err(1), str, Err(1))])
 def test_map(obj, call, exp):
     assert obj.map(call) == exp
 
 
-@parametrize('obj,call,exp', [
-    (Ok(1), str, Ok(1)),
-    (Err(1), str, Err('1'))
-])
+@parametrize("obj,call,exp", [(Ok(1), str, Ok(1)), (Err(1), str, Err("1"))])
 def test_map_err(obj, call, exp):
     assert obj.map_err(call) == exp
 
 
-@parametrize('obj,exp,ok', [
-    (Ok(1), 1, True),
-    (Ok(None), None, True),
-    (Err(1), 1, False),
-])
+@parametrize(
+    "obj,exp,ok",
+    [
+        (Ok(1), 1, True),
+        (Ok(None), None, True),
+        (Err(1), 1, False),
+    ],
+)
 def test_unwrap(obj, exp, ok):
     if ok:
         assert obj.unwrap() == exp
@@ -97,55 +84,56 @@ def test_unwrap(obj, exp, ok):
             obj.unwrap()
 
 
-@parametrize('obj,optb,exp', [
-    (Ok(0), 11, 0),
-    (Err(11), 0, 0)
-])
+@parametrize("obj,optb,exp", [(Ok(0), 11, 0), (Err(11), 0, 0)])
 def test_unwrap_or(obj, optb, exp):
     assert obj.unwrap_or(optb) == exp
 
 
-@parametrize('obj,op,exp', [
-    (Ok('asd'), len, 'asd'),
-    (Err('asd'), len, 3),
-])
+@parametrize(
+    "obj,op,exp",
+    [
+        (Ok("asd"), len, "asd"),
+        (Err("asd"), len, 3),
+    ],
+)
 def test_unwrap_or_else(obj, op, exp):
     assert obj.unwrap_or_else(op) == exp
 
 
-@parametrize('obj,ok,exp', [
-    (Ok(1), True, 1),
-    (Err(1), False, ''),
-    (Ok(None), True, None)
-])
+@parametrize(
+    "obj,ok,exp",
+    [(Ok(1), True, 1), (Err(1), False, ""), (Ok(None), True, None)],
+)
 def test_except(obj, ok, exp):
     if ok:
-        assert obj.expect('') == exp
+        assert obj.expect("") == exp
     else:
         with pytest.raises(ValueError):
-            obj.expect('')
+            obj.expect("")
 
 
-@parametrize('obj,err,exp', [
-    (Ok(1), False, ''),
-    (Err(None), True, None),
-])
+@parametrize(
+    "obj,err,exp",
+    [
+        (Ok(1), False, ""),
+        (Err(None), True, None),
+    ],
+)
 def test_unwrap_expect_err(obj, err, exp):
     if err:
         assert obj.unwrap_err() == exp
-        assert obj.expect_err('') == exp
+        assert obj.expect_err("") == exp
     else:
         with pytest.raises(ValueError):
             obj.unwrap_err()
         with pytest.raises(ValueError):
-            obj.expect_err('')
+            obj.expect_err("")
 
 
-@parametrize('obj1,obj2,eq', [
-    (Ok(1), Ok(1), True),
-    (Err(1), Err(1), True),
-    (Ok(1), Err(1), False)
-])
+@parametrize(
+    "obj1,obj2,eq",
+    [(Ok(1), Ok(1), True), (Err(1), Err(1), True), (Ok(1), Err(1), False)],
+)
 def test_hash(obj1, obj2, eq):
     if eq:
         assert hash(obj1) == hash(obj2)
@@ -153,36 +141,38 @@ def test_hash(obj1, obj2, eq):
         assert hash(obj1) != hash(obj2)
 
 
-@parametrize('o1,o2', [
-    (Ok(''), Ok('')),
-    (Ok([]), Ok([])),
-    (Err('aa'), Err('aa')),
-    (Err({}), Err({}))
-])
+@parametrize(
+    "o1,o2",
+    [
+        (Ok(""), Ok("")),
+        (Ok([]), Ok([])),
+        (Err("aa"), Err("aa")),
+        (Err({}), Err({})),
+    ],
+)
 def test_eq(o1, o2):
     assert o1 == o2
     assert not o1 == o1._val
     assert not o1._val == o1
 
 
-@parametrize('o1,o2', [
-    (Ok(''), Err('')),
-    (Err([]), Ok([])),
-    (Ok({}), Err({})),
-    (Ok(1), Ok(2)),
-    (Err(2), Err(3))
-])
+@parametrize(
+    "o1,o2",
+    [
+        (Ok(""), Err("")),
+        (Err([]), Ok([])),
+        (Ok({}), Err({})),
+        (Ok(1), Ok(2)),
+        (Err(2), Err(3)),
+    ],
+)
 def test_neq(o1, o2):
     assert o1 != o2
     assert o1 != o1._val
     assert o1._val != o1
 
 
-@parametrize('o1,o2', [
-    (Ok(2), Err(1)),
-    (Ok(1), Ok(2)),
-    (Err(1), Err(2))
-])
+@parametrize("o1,o2", [(Ok(2), Err(1)), (Ok(1), Ok(2)), (Err(1), Err(2))])
 def test_lt_gt(o1, o2):
     assert o1 < o2
     assert o1 <= o2
@@ -190,23 +180,23 @@ def test_lt_gt(o1, o2):
     assert o2 >= o1
 
 
-@parametrize('o1,o2', [
-    (Ok(1), Ok(1)),
-    (Err(1), Err(1))
-])
+@parametrize("o1,o2", [(Ok(1), Ok(1)), (Err(1), Err(1))])
 def test_le_ge(o1, o2):
     assert o1 <= o2
     assert o1 >= o2
 
 
-@parametrize('self,other', [
-    (Err(None), Err(1)),
-    (Ok(1), Ok(None)),
-    (Ok(1), Ok('')),
-    (Ok(''), Ok(None)),
-    (Ok(1), 1),
-    (1, Err(1))
-])
+@parametrize(
+    "self,other",
+    [
+        (Err(None), Err(1)),
+        (Ok(1), Ok(None)),
+        (Ok(1), Ok("")),
+        (Ok(""), Ok(None)),
+        (Ok(1), 1),
+        (1, Err(1)),
+    ],
+)
 def test_lt_gt_type_error(self, other):
     with pytest.raises(TypeError):
         self < other
