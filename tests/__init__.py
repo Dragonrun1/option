9ee8465ef02da1fdf6bb0a23b3d21d38facb1c15
Michael Cummings <mgcummings@yahoo.com>
2025-09-22 19:36:00 -0700

    Update LICENSE headers, improve docstrings, add comparison protocol methods, and refactor linting settings.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,12 @@
 # Copyright © 2025 Michael Cummings <mgcummings@yahoo.com>
 #
 # Licensed under the MIT license
-# [MIT](https:#opensource.org/license/mit-0)
+# [MIT](https://opensource.org/license/mit-0)
 #
 # Files in this project may not be copied, modified, or distributed except
 # according to those terms.
 #
-# The full test of the license can be found in the project LICENSE.md file.
+# The full text of the license can be found in the project LICENSE.md file.
 #
 # SPDX-License-Identifier: MIT
 ##############################################################################
